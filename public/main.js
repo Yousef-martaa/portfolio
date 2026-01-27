c6bce898fe392f91163311a64ff8c92014a1647f
Alkendi-1 <a.alkendi26@gmail.com>
2026-01-28 00:41:44 +0100

    Update projects layout and responsive cards

--- public/main.js
+++ public/main.js
@@ -1,18 +1,15 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+fetch("/api/projects")
+  .then(res => res.json())
+  .then(projects => {
+    const container = document.getElementById("projects");
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
-
-// Main route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Projects API 
-app.get("/api/projects", (req, res) => {
-  res.json([]);
-});
-
-module.exports = app;
+    projects.forEach(p => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <h3>${p.title}</h3>
+        <p>${p.description}</p>
+        <small>${p.tech}</small>
+      `;
+      container.appendChild(div);
+    });
+  });
